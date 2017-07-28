89f7362a9238097fc666ff4c68f6843b2d8500e0
Kennon Houck <kennon_h@hotmail.com>
2017-07-27 18:05:05 -0700

    CreatePage initial commit (#5)

--- app/components/CreatePage/CreatePage.js
+++ app/components/CreatePage/CreatePage.js
@@ -1,20 +1,42 @@
-import React, { Component } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from "react";
+import { AppRegistry, Button, Text, TextInput, View } from "react-native";
 
-// Components
+import Header from '../Common/Header';
 
 
-class CreatePage extends Component {
-    static navigationOptions = {
-        title: 'Create New Workout',
-    };
-    render() {
-        return (
+export default class CreatePage extends Component{
+    constructor(props) {
+        super(props);
+        this.state= {text:''};
+    }
+
+    btnPress(){
+        Alert.alert('Alert Ttle', alertMessage,[
+            {text: 'Cancel', onPress: () => console.log('Cancel Pressed!')},
+            {text: 'Ok', onPress: () => console.log('0k Pressed')},
+        ])
+        
+    }
+    render(){
+        return(
             <View>
-                <Text>Create Page</Text>
+                <Header />
+                <Text>Workout Name</Text>
+                <TextInput
+                    style={{height: 20}}
+                    placeholder="Workout Name Here"
+                    onChangeText={(text) =>this.setState({text}) />
+                <Text>Description</Text>  
+                <TextInput
+                    style={{height: 20}}
+                    placeholder="Description (optional)"
+                    onChangeText={(text) =>this.setState({text}) />
+                <Button
+                    onPress={this.btnPress}
+                    title="Submit"
+                    color="blue"
+                    accessibilityLabel="This is a standard button"
             </View>
         )
     }
 }
-
-export default CreatePage;
